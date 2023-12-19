8e4a85d740694a97a436acd6e5771e7192cb5bf1
TtroubleTT <TtroubleTT2@gmail.com>
2023-12-18 21:23:28 -0800

    animation manager

--- Christmas Game Jam/Assets/Scripts/Animation/AnimationManager.cs
+++ Christmas Game Jam/Assets/Scripts/Animation/AnimationManager.cs
@@ -4,5 +4,14 @@ using UnityEngine;
 
 public class AnimationManager : MonoBehaviour
 {
-
+    [SerializeField] private Animator animator;
+    
+    public enum AnimationType
+    {
+        WalkForward,
+        WalkBackward,
+        WalkRight,
+        WalkLeft,
+        Crouch,
+    }
 }
