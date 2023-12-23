b57f3c9485d9250f476077971eb99487918f9dfa
TtroubleTT <TtroubleTT2@gmail.com>
2023-12-23 02:26:08 -0800

    santa will now shoot while he chases u

--- Christmas Game Jam/Assets/Scripts/Entity/Enemy/SaintClau.cs
+++ Christmas Game Jam/Assets/Scripts/Entity/Enemy/SaintClau.cs
@@ -25,6 +25,7 @@ public class SaintClau : EnemyBase
     [SerializeField] private float shotRange = 120f;
     [SerializeField] private float shotCooldown = 3f;
     private float _lastAttackTime;
+    private float _lastShotTime;
     
     [Header("Projectile Stats")] 
     [SerializeField] private float damage = 10f;
@@ -34,9 +35,12 @@ public class SaintClau : EnemyBase
     [Header("References")] 
     [SerializeField] private CharacterController controller;
     [SerializeField] private AnimationManager animationManager;
-    [SerializeField] private Animator animator;
+    [SerializeField] private Transform shootTransformation;
+    [SerializeField] private GameObject projectilePrefab;
     private PlayerBase _playerBase;
 
+    private readonly Dictionary<ShootingEnemy.Stats, float> _projectileStats = new();
+    
     protected override void InitializeAbstractedStats()
     {
         MaxHealth = maxHealth;
@@ -46,10 +50,19 @@ public class SaintClau : EnemyBase
         CharacterController = controller;
     }
     
+    private void InitializeStats()
+    {
+        _projectileStats.Add(ShootingEnemy.Stats.Damage, damage);
+        _projectileStats.Add(ShootingEnemy.Stats.Speed, speed);
+        _projectileStats.Add(ShootingEnemy.Stats.Range, range);
+    }
+    
     private void Start()
     {
+        InitializeStats();
         InitializeAbstractedStats();
         _playerBase = Player.GetComponent<PlayerBase>();
+        animationManager.PlaySantaAnimation(AnimationManager.SantaAnimationType.Beam);
     }
     
     private void Update()
@@ -57,7 +70,7 @@ public class SaintClau : EnemyBase
         Gravity();
         LookAtPlayer();
         MoveTowardsPlayer();
-        CheckAttack();
+        CheckShoot();
     }
 
     private bool IsInMeleeRange()
@@ -87,5 +100,48 @@ public class SaintClau : EnemyBase
     private void Attack()
     {
         _playerBase.SubtractHealth(meleeDamage);
+    }
+    
+    // Checks if the distance between player and enemy is within the range they are allowed to fire
+    private bool IsInShotRange()
+    {
+        float distance = Vector3.Distance(Player.transform.position, transform.position);
+
+        if (distance <= shotRange)
+            return true;
+
+        return false;
+    }
+
+    // Checks if the player is within the enemies line of sight
+    private bool InLineOfSight()
+    {
+        if (Physics.Raycast(transform.position + transform.up, (Player.transform.position - transform.position), out RaycastHit hitInfo, shotRange))
+        {
+            if (hitInfo.transform.gameObject == Player)
+            {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    // If the shot cooldown has passed, and the player is within shooting range, and line of sight then shoot
+    private void CheckShoot()
+    {
+        if (Time.time - _lastShotTime > shotCooldown && IsInShotRange() && InLineOfSight())
+        {
+            _lastShotTime = Time.time;
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        Transform myTransform = shootTransformation;
+        GameObject projectile = Instantiate(projectilePrefab, myTransform.position + myTransform.forward + myTransform.up, myTransform.rotation);
+        Vector3 direction = (Player.transform.position - transform.position).normalized; // Gets direction of player
+        projectile.GetComponent<ShootingProjectile>().ProjectileInitialize(_projectileStats, direction);
     }
 }
